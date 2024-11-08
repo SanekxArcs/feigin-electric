bd72b0614e9de63ff7d566179e82bfdf0a210d4f
Oleksandr Dzisiak <sanek.arc@gmail.com>
2024-11-08 17:42:19 +0100

    Update page.tsx

--- nextjs-feigin/src/app/blog/page.tsx
+++ nextjs-feigin/src/app/blog/page.tsx
@@ -7,7 +7,7 @@ import { Image } from "next-sanity/image";
 const POSTS_QUERY = `*[
   _type == "post"
   && defined(slug.current)
-]|order(publishedAt desc)[0...12]{_id, title, mainImage, slug, publishedAt}`;
+]|order(publishedAt desc)[0...12]{_id, title, mainImage, body, slug, publishedAt}`;
 
 const options = { next: { revalidate: 30 } };
 
@@ -26,21 +26,33 @@ export default async function IndexPage() {
       <h1 className="text-4xl font-bold mb-8">Posts</h1>
       <ul className="grid gap-4 grid-cols-2">
         {posts.map((post) => (
-          <li
-            className="hover:underline hover:scale-105 transition-all bg-white rounded-xl grid gap-4 p-5 text-black"
-            key={post._id}
-          ><Link href={`/${post.slug.current}`}>
-            <Image
-              src={urlFor(post.mainImage.asset).url()}
-              alt={post.title}
-              width={500}
-              height={300}
-              objectFit="cover"
-              className="rounded-lg"
-            />
-            
-              <h2 className="text-xl font-semibold">{post.title}</h2>
-              <p>{new Date(post.publishedAt).toLocaleDateString()}</p>
+          <li key={post._id}>
+            <Link href={`/${post.slug.current}`}>
+              <article className="overflow-hidden rounded-lg shadow transition hover:shadow-lg dark:shadow-gray-700/25">
+                <Image
+                  src={urlFor(post.mainImage.asset).url()}
+                  alt={post.title}
+                  width={500}
+                  height={300}
+                  objectFit="cover"
+                  className="h-56 w-full object-cover"
+                />
+                <div className="bg-white p-4 sm:p-6 dark:bg-gray-900">
+                  <time
+                    dateTime="2022-10-10"
+                    className="block text-xs text-gray-500 dark:text-gray-400"
+                  >
+                    {new Date(post.publishedAt).toLocaleDateString()}
+                  </time>
+
+                  <h3 className="mt-0.5 text-lg text-gray-900 dark:text-white">
+                    {post.title}
+                  </h3>
+                  {post.body.block ? <p className="mt-2 line-clamp-3 text-sm/relaxed text-gray-500 dark:text-gray-400">
+                    {post.body.block}
+                  </p> : ""}
+                </div>
+              </article>
             </Link>
           </li>
         ))}
