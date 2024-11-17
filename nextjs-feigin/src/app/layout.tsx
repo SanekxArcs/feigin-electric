73d5eda538517d24389045e65583c12b731d11e8
Oleksandr Dzisiak <sanek.arc@gmail.com>
2024-11-17 22:40:21 +0100

    add speed insight metrics

--- nextjs-feigin/src/app/layout.tsx
+++ nextjs-feigin/src/app/layout.tsx
@@ -1,5 +1,6 @@
 
 import type { Metadata } from "next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import localFont from "next/font/local";
 import "./globals.css";
 import Header from "./components/Header";
